cb5de5149870a37fc052c323a933e451c602b846
Luigi Agosti <luigi.agosti@gmail.com>
2012-01-28 15:35:37 +0000

    using value from a temporary intent to generate the view

--- src/com/giago/ecard/activity/Show.java
+++ src/com/giago/ecard/activity/Show.java
@@ -1,14 +1,15 @@
 package com.giago.ecard.activity;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
-
-import com.giago.ecard.R;
+import java.io.IOException;
+import java.io.InputStream;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+
+import com.giago.ecard.R;
 
 public class Show extends Activity {
 
@@ -20,35 +21,55 @@ public class Show extends Activity {
 	
 	@Override
 	protected void onResume() {
-		super.onResume();
+		super.onResume();		
+		Intent i = createIntent();
 		
-		String company = encodeParameter("Giago Software Ltd");
-		String name = encodeParameter("Luigi Agosti");
-		String phone = encodeParameter("M: +4492830233");
-		String email = encodeParameter("admin@gi-ago.com");
-		String note = encodeParameter("T: @luigiagosti");
-		String qrcode = encodeParameter("https://plus.google.com/102048052961229488268/posts");
+		String template = readAsset("basic_blue.html");
+		template = replaceParamFromIntent(i, "company", template);
+		template = replaceParamFromIntent(i, "name", template);
+		template = replaceParamFromIntent(i, "phone", template);
+		template = replaceParamFromIntent(i, "email", template);
+		template = replaceParamFromIntent(i, "note", template);
+		template = replaceParamFromIntent(i, "role", template);
+		template = replaceParamFromIntent(i, "qrdata", template);
 		
 		WebView wv = (WebView)findViewById(R.id.ecardwebview);
 		wv.getSettings().setJavaScriptEnabled(true);
-		wv.loadUrl("file:///android_asset/ecard.html?company="+ company 
-				+ "&name=" + name + "&phone=" + phone + "&email=" 
-				+ email + "&note=" + note + "&qrcode=" + qrcode);
+		wv.loadDataWithBaseURL("file:///android_asset/", 
+				template, "text/html", "utf-8", "file:///android_asset/");
 		wv.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 	}
-	
-	@Override
-	protected void onPause() {
-		super.onPause();
-	}
 
-	private static final String ENCODING = "UTF-8";
+	private Intent createIntent() {
+		Intent i = new Intent("example");
+		i.putExtra("company", "Giago Software Ltd");
+		i.putExtra("role", "Developer");
+		i.putExtra("name", "Luigi Agosti");
+		i.putExtra("phone", "M: +4492830233");
+		i.putExtra("email", "admin@gi-ago.com");
+		i.putExtra("note", "T: @luigiagosti");
+		i.putExtra("qrdata", "https://plus.google.com/102048052961229488268/posts");
+		return i;
+	}
 	
-	protected String encodeParameter(String parameter) {
-		try {
-			return URLEncoder.encode(parameter, ENCODING);
-		} catch (UnsupportedEncodingException e) {
-			return "";
+	private String replaceParamFromIntent(Intent i, String param, String data) {
+		String value = i.getStringExtra(param);
+		if(value == null) {
+			value = "";
 		}
+		return data.replaceAll(param + "value", value);
+	}
+	
+	private String readAsset(String filename) {
+		try {
+	        InputStream is = getAssets().open(filename);
+	        int size = is.available();
+	        byte[] buffer = new byte[size];
+	        is.read(buffer);
+	        is.close();
+	        return new String(buffer);
+		} catch (IOException e) {
+	        throw new RuntimeException(e);
+	    }
 	}
 }
