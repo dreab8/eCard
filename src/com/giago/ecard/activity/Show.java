cb3792b1a64eb56292c1279047f9b00242e10b45
Luigi Agosti <luigi.agosti@gmail.com>
2012-01-28 12:19:02 +0000

    new activity for ecard details

---
+++ src/com/giago/ecard/activity/Show.java
@@ -0,0 +1,16 @@
+package com.giago.ecard.activity;
+
+import com.giago.ecard.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Show extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.show);
+	}
+	
+}
