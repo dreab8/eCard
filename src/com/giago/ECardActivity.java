d64fedcc52040c697da1f51e2d3409750912a21f
Andrea Boriero <dreborier@gmail.com>
2012-01-28 11:10:58 +0000

    project setup

---
+++ src/com/giago/ECardActivity.java
@@ -0,0 +1,13 @@
+package com.giago;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ECardActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
